8d93531a5e9b29647f524cd32f54ecbf92755593
lhc <liuhuicheng1@kingsoft.com>
2022-05-24 14:10:55 +0800

    bill-union

--- ksyun/client/bill_union/v20200101/models.py
+++ ksyun/client/bill_union/v20200101/models.py
@@ -6,10 +6,19 @@ class DescribeBillSummaryByPayModeRequest(AbstractModel):
 
     def __init__(self):
         r"""获取计费类别汇总账单
+        :param BillBeginMonth: 账单开始时间
+        :type PathPrefix: String
+        :param BillEndMonth: 账单结束时间
+        :type PathPrefix: String
         """
+        self.BillBeginMonth = None
+        self.BillEndMonth = None
 
     def _deserialize(self, params):
-        return
+        if params.get("BillBeginMonth"):
+            self.BillBeginMonth = params.get("BillBeginMonth")
+        if params.get("BillEndMonth"):
+            self.BillEndMonth = params.get("BillEndMonth")
 
 
 class DescribeBillSummaryByProductRequest(AbstractModel):
@@ -18,10 +27,19 @@ class DescribeBillSummaryByProductRequest(AbstractModel):
 
     def __init__(self):
         r"""按产品线获取账单汇总金额
+        :param BillBeginMonth: 账单开始时间
+        :type PathPrefix: String
+        :param BillEndMonth: 账单结束时间
+        :type PathPrefix: String
         """
+        self.BillBeginMonth = None
+        self.BillEndMonth = None
 
     def _deserialize(self, params):
-        return
+        if params.get("BillBeginMonth"):
+            self.BillBeginMonth = params.get("BillBeginMonth")
+        if params.get("BillEndMonth"):
+            self.BillEndMonth = params.get("BillEndMonth")
 
 
 class DescribeBillSummaryByProjectRequest(AbstractModel):
@@ -51,10 +69,29 @@ class DescribeInstanceSummaryBillsRequest(AbstractModel):
 
     def __init__(self):
         r"""按实例ID获取账单汇总金额
+        :param BillMonth: 客户账单月份，YYYY-MM。不支持跨月查询
+        :type PathPrefix: String
+        :param ProductCode: 产品线对应的Code
+        :type PathPrefix: String
+        :param Page: 页码
+        :type PathPrefix: Int
+        :param Size: 每页数量
+        :type PathPrefix: Int
         """
+        self.BillMonth = None
+        self.ProductCode = None
+        self.Page = None
+        self.Size = None
 
     def _deserialize(self, params):
-        return
+        if params.get("BillMonth"):
+            self.BillMonth = params.get("BillMonth")
+        if params.get("ProductCode"):
+            self.ProductCode = params.get("ProductCode")
+        if params.get("Page"):
+            self.Page = params.get("Page")
+        if params.get("Size"):
+            self.Size = params.get("Size")
 
 
 class DescribeProductCodeRequest(AbstractModel):
@@ -75,9 +112,43 @@ class DescribeSplitItemBillDetailsRequest(AbstractModel):
 
     def __init__(self):
         r"""分页查询分拆项账单明细
+        :param CustomerBillMonth: 账期，格式：yyyyMM。如：202111
+        :type PathPrefix: Int
+        :param ProductGroupCode: 产品线 code
+        :type PathPrefix: String
+        :param StatisticType: 统计周期
+        :type PathPrefix: Int
+        :param PayType: 计费类别
+        :type PathPrefix: Int
+        :param SubAccountId: 主账号所托管的账号Id
+        :type PathPrefix: Int
+        :param Page: 第几页
+        :type PathPrefix: Int
+        :param Size: 每页条数
+        :type PathPrefix: String
         """
+        self.CustomerBillMonth = None
+        self.ProductGroupCode = None
+        self.StatisticType = None
+        self.PayType = None
+        self.SubAccountId = None
+        self.Page = None
+        self.Size = None
 
     def _deserialize(self, params):
-        return
+        if params.get("CustomerBillMonth"):
+            self.CustomerBillMonth = params.get("CustomerBillMonth")
+        if params.get("ProductGroupCode"):
+            self.ProductGroupCode = params.get("ProductGroupCode")
+        if params.get("StatisticType"):
+            self.StatisticType = params.get("StatisticType")
+        if params.get("PayType"):
+            self.PayType = params.get("PayType")
+        if params.get("SubAccountId"):
+            self.SubAccountId = params.get("SubAccountId")
+        if params.get("Page"):
+            self.Page = params.get("Page")
+        if params.get("Size"):
+            self.Size = params.get("Size")
 
 
--- ksyun/client/bill_union/v20211209/models.py
+++ ksyun/client/bill_union/v20211209/models.py
@@ -6,9 +6,33 @@ class DescribeCostBillRequest(AbstractModel):
 
     def __init__(self):
         r"""获取预付费成本账单
+        :param billMonth: 账单月
+        :type PathPrefix: Int
+        :param statisticalItem: 统计项，0：按实例；1：按产品
+        :type PathPrefix: Int
+        :param instanceIds: 实例id列表，仅当statisticalItem=0时有效
+        :type PathPrefix: String
+        :param pageNo: 分页page no
+        :type PathPrefix: Int
+        :param pageSize: 分页page size
+        :type PathPrefix: Int
         """
+        self.billMonth = None
+        self.statisticalItem = None
+        self.instanceIds = None
+        self.pageNo = None
+        self.pageSize = None
 
     def _deserialize(self, params):
-        return
+        if params.get("billMonth"):
+            self.billMonth = params.get("billMonth")
+        if params.get("statisticalItem"):
+            self.statisticalItem = params.get("statisticalItem")
+        if params.get("instanceIds"):
+            self.instanceIds = params.get("instanceIds")
+        if params.get("pageNo"):
+            self.pageNo = params.get("pageNo")
+        if params.get("pageSize"):
+            self.pageSize = params.get("pageSize")
 
 

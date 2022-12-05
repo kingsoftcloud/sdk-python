500e48ef8505c26282eb1c22b6cb08f04de7848d
lhc <liuhuicheng1@kingsoft.com>
2022-12-05 18:44:56 +0800

    tagv2

---
+++ ksyun/client/tagv2/v20200901/client.py
@@ -0,0 +1,194 @@
+import json
+
+from ksyun.common.exception.ksyun_sdk_exception import KsyunSDKException
+from ksyun.common.abstract_client import AbstractClient
+
+
+class Tagv2Client(AbstractClient):
+    _apiVersion = '2020-09-01'
+    _endpoint = 'tagv2.api.ksyun.com'
+    _service = 'tagv2'
+    def CreateTag(self, request):
+        """创建标签
+        :param request: Request instance for CreateTag.
+        :type request: :class:`ksyun.client.tagv2.v20200901.models.CreateTagRequest`
+        """
+        try:
+            params = request._serialize()
+            body = self.call("CreateTag", params)
+            response = json.loads(body)
+            if "Error" not in response:
+                return body
+            else:
+                code = response["Error"]["Code"]
+                message = response["Error"]["Message"]
+                req_id = response["RequestId"]
+                raise KsyunSDKException(code, message, req_id)
+        except Exception as e:
+            if isinstance(e, KsyunSDKException):
+                raise
+            else:
+                raise KsyunSDKException(e.message, e.message)
+
+
+    def DeleteTag(self, request):
+        """删除标签
+        :param request: Request instance for DeleteTag.
+        :type request: :class:`ksyun.client.tagv2.v20200901.models.DeleteTagRequest`
+        """
+        try:
+            params = request._serialize()
+            body = self.call("DeleteTag", params)
+            response = json.loads(body)
+            if "Error" not in response:
+                return body
+            else:
+                code = response["Error"]["Code"]
+                message = response["Error"]["Message"]
+                req_id = response["RequestId"]
+                raise KsyunSDKException(code, message, req_id)
+        except Exception as e:
+            if isinstance(e, KsyunSDKException):
+                raise
+            else:
+                raise KsyunSDKException(e.message, e.message)
+
+
+    def ListTags(self, request):
+        """获取标签列表
+        :param request: Request instance for ListTags.
+        :type request: :class:`ksyun.client.tagv2.v20200901.models.ListTagsRequest`
+        """
+        try:
+            params = request._serialize()
+            body = self.call("ListTags", params)
+            response = json.loads(body)
+            if "Error" not in response:
+                return body
+            else:
+                code = response["Error"]["Code"]
+                message = response["Error"]["Message"]
+                req_id = response["RequestId"]
+                raise KsyunSDKException(code, message, req_id)
+        except Exception as e:
+            if isinstance(e, KsyunSDKException):
+                raise
+            else:
+                raise KsyunSDKException(e.message, e.message)
+
+
+    def ListTagKeys(self, request):
+        """获取标签键列表
+        :param request: Request instance for ListTagKeys.
+        :type request: :class:`ksyun.client.tagv2.v20200901.models.ListTagKeysRequest`
+        """
+        try:
+            params = request._serialize()
+            body = self.call("ListTagKeys", params)
+            response = json.loads(body)
+            if "Error" not in response:
+                return body
+            else:
+                code = response["Error"]["Code"]
+                message = response["Error"]["Message"]
+                req_id = response["RequestId"]
+                raise KsyunSDKException(code, message, req_id)
+        except Exception as e:
+            if isinstance(e, KsyunSDKException):
+                raise
+            else:
+                raise KsyunSDKException(e.message, e.message)
+
+
+    def ListTagValues(self, request):
+        """获取标签值列表
+        :param request: Request instance for ListTagValues.
+        :type request: :class:`ksyun.client.tagv2.v20200901.models.ListTagValuesRequest`
+        """
+        try:
+            params = request._serialize()
+            body = self.call("ListTagValues", params)
+            response = json.loads(body)
+            if "Error" not in response:
+                return body
+            else:
+                code = response["Error"]["Code"]
+                message = response["Error"]["Message"]
+                req_id = response["RequestId"]
+                raise KsyunSDKException(code, message, req_id)
+        except Exception as e:
+            if isinstance(e, KsyunSDKException):
+                raise
+            else:
+                raise KsyunSDKException(e.message, e.message)
+
+
+    def ListResources(self, request):
+        """获取用户资源列表
+        :param request: Request instance for ListResources.
+        :type request: :class:`ksyun.client.tagv2.v20200901.models.ListResourcesRequest`
+        """
+        try:
+            params = request._serialize()
+            body = self.call("ListResources", params)
+            response = json.loads(body)
+            if "Error" not in response:
+                return body
+            else:
+                code = response["Error"]["Code"]
+                message = response["Error"]["Message"]
+                req_id = response["RequestId"]
+                raise KsyunSDKException(code, message, req_id)
+        except Exception as e:
+            if isinstance(e, KsyunSDKException):
+                raise
+            else:
+                raise KsyunSDKException(e.message, e.message)
+
+
+    def ReplaceResourcesTags(self, request):
+        """批量替换资源的全部标签
+        :param request: Request instance for ReplaceResourcesTags.
+        :type request: :class:`ksyun.client.tagv2.v20200901.models.ReplaceResourcesTagsRequest`
+        """
+        try:
+            params = request._serialize()
+            body = self.call("ReplaceResourcesTags", params)
+            response = json.loads(body)
+            if "Error" not in response:
+                return body
+            else:
+                code = response["Error"]["Code"]
+                message = response["Error"]["Message"]
+                req_id = response["RequestId"]
+                raise KsyunSDKException(code, message, req_id)
+        except Exception as e:
+            if isinstance(e, KsyunSDKException):
+                raise
+            else:
+                raise KsyunSDKException(e.message, e.message)
+
+
+    def DetachResourceTags(self, request):
+        """解绑资源标签
+        :param request: Request instance for DetachResourceTags.
+        :type request: :class:`ksyun.client.tagv2.v20200901.models.DetachResourceTagsRequest`
+        """
+        try:
+            params = request._serialize()
+            body = self.call("DetachResourceTags", params)
+            response = json.loads(body)
+            if "Error" not in response:
+                return body
+            else:
+                code = response["Error"]["Code"]
+                message = response["Error"]["Message"]
+                req_id = response["RequestId"]
+                raise KsyunSDKException(code, message, req_id)
+        except Exception as e:
+            if isinstance(e, KsyunSDKException):
+                raise
+            else:
+                raise KsyunSDKException(e.message, e.message)
+
+
---
+++ ksyun/client/tagv2/v20200901/models.py
@@ -0,0 +1,220 @@
+from ksyun.common.abstract_model import AbstractModel
+
+class CreateTagRequest(AbstractModel):
+    """CreateTag请求参数结构体
+    """
+
+    def __init__(self):
+        r"""创建标签
+        :param Key: Key 最多128字符，仅支持中英文字符/数字+-=._:/@，不能以ksc开头
+        :type PathPrefix: String
+        :param Value: Value最多256字符，仅支持中英文字符/数字+-=._:/@(){}，不能以ksc开头,多个以","隔开
+        :type PathPrefix: String
+        """
+        self.Key = None
+        self.Value = None
+
+    def _deserialize(self, params):
+        if params.get("Key"):
+            self.Key = params.get("Key")
+        if params.get("Value"):
+            self.Value = params.get("Value")
+
+
+class DeleteTagRequest(AbstractModel):
+    """DeleteTag请求参数结构体
+    """
+
+    def __init__(self):
+        r"""删除标签
+        :param Tags: Tags
+        :type PathPrefix: Array
+        """
+        self.Tags = None
+
+    def _deserialize(self, params):
+        if params.get("Tags"):
+            self.Tags = params.get("Tags")
+
+
+class ListTagsRequest(AbstractModel):
+    """ListTags请求参数结构体
+    """
+
+    def __init__(self):
+        r"""获取标签列表
+        :param Page: Page
+        :type PathPrefix: Int
+        :param PageSize: PageSize
+        :type PathPrefix: Int
+        :param Key: key
+        :type PathPrefix: String
+        :param Value: Value
+        :type PathPrefix: String
+        """
+        self.Page = None
+        self.PageSize = None
+        self.Key = None
+        self.Value = None
+
+    def _deserialize(self, params):
+        if params.get("Page"):
+            self.Page = params.get("Page")
+        if params.get("PageSize"):
+            self.PageSize = params.get("PageSize")
+        if params.get("Key"):
+            self.Key = params.get("Key")
+        if params.get("Value"):
+            self.Value = params.get("Value")
+
+
+class ListTagKeysRequest(AbstractModel):
+    """ListTagKeys请求参数结构体
+    """
+
+    def __init__(self):
+        r"""获取标签键列表
+        :param TagKey: 标签键名称
+        :type PathPrefix: String
+        :param Page: 页码
+        :type PathPrefix: Int
+        :param PageSize: 每页数据量
+        :type PathPrefix: Int
+        """
+        self.TagKey = None
+        self.Page = None
+        self.PageSize = None
+
+    def _deserialize(self, params):
+        if params.get("TagKey"):
+            self.TagKey = params.get("TagKey")
+        if params.get("Page"):
+            self.Page = params.get("Page")
+        if params.get("PageSize"):
+            self.PageSize = params.get("PageSize")
+
+
+class ListTagValuesRequest(AbstractModel):
+    """ListTagValues请求参数结构体
+    """
+
+    def __init__(self):
+        r"""获取标签值列表
+        :param TagKeys: 标签键名称，多个用逗号连接
+        :type PathPrefix: String
+        :param Page: 页码
+        :type PathPrefix: Int
+        :param PageSize: 页面限制
+        :type PathPrefix: Int
+        """
+        self.TagKeys = None
+        self.Page = None
+        self.PageSize = None
+
+    def _deserialize(self, params):
+        if params.get("TagKeys"):
+            self.TagKeys = params.get("TagKeys")
+        if params.get("Page"):
+            self.Page = params.get("Page")
+        if params.get("PageSize"):
+            self.PageSize = params.get("PageSize")
+
+
+class ListResourcesRequest(AbstractModel):
+    """ListResources请求参数结构体
+    """
+
+    def __init__(self):
+        r"""获取用户资源列表
+        :param ResourceType: 资源类型英文简称
+        :type PathPrefix: String
+        :param ProjectIds: 项目ID，多个用逗号连接
+        :type PathPrefix: String
+        :param RegionCodes: regioncode，多个用逗号连接
+        :type PathPrefix: String
+        :param ResourceUuids: 资源ID，多个用逗号连接
+        :type PathPrefix: String
+        :param ResourceName: 资源类型名称
+        :type PathPrefix: String
+        :param TagFilters: 
+        :type PathPrefix: Array
+        :param Page: 页码
+        :type PathPrefix: Int
+        :param PageSize: 分页数
+        :type PathPrefix: Int
+        """
+        self.ResourceType = None
+        self.ProjectIds = None
+        self.RegionCodes = None
+        self.ResourceUuids = None
+        self.ResourceName = None
+        self.TagFilters = None
+        self.Page = None
+        self.PageSize = None
+
+    def _deserialize(self, params):
+        if params.get("ResourceType"):
+            self.ResourceType = params.get("ResourceType")
+        if params.get("ProjectIds"):
+            self.ProjectIds = params.get("ProjectIds")
+        if params.get("RegionCodes"):
+            self.RegionCodes = params.get("RegionCodes")
+        if params.get("ResourceUuids"):
+            self.ResourceUuids = params.get("ResourceUuids")
+        if params.get("ResourceName"):
+            self.ResourceName = params.get("ResourceName")
+        if params.get("TagFilters"):
+            self.TagFilters = params.get("TagFilters")
+        if params.get("Page"):
+            self.Page = params.get("Page")
+        if params.get("PageSize"):
+            self.PageSize = params.get("PageSize")
+
+
+class ReplaceResourcesTagsRequest(AbstractModel):
+    """ReplaceResourcesTags请求参数结构体
+    """
+
+    def __init__(self):
+        r"""批量替换资源的全部标签
+        :param ResourceType: 资源类型英文简写
+        :type PathPrefix: String
+        :param ReplaceTags: 
+        :type PathPrefix: Array
+        """
+        self.ResourceType = None
+        self.ReplaceTags = None
+
+    def _deserialize(self, params):
+        if params.get("ResourceType"):
+            self.ResourceType = params.get("ResourceType")
+        if params.get("ReplaceTags"):
+            self.ReplaceTags = params.get("ReplaceTags")
+
+
+class DetachResourceTagsRequest(AbstractModel):
+    """DetachResourceTags请求参数结构体
+    """
+
+    def __init__(self):
+        r"""解绑资源标签
+        :param ResourceType: 资源类型英文简写
+        :type PathPrefix: String
+        :param ResourceUuid: 资源ID
+        :type PathPrefix: String
+        :param TagIds: 标签值ID，多个用逗号连接
+        :type PathPrefix: String
+        """
+        self.ResourceType = None
+        self.ResourceUuid = None
+        self.TagIds = None
+
+    def _deserialize(self, params):
+        if params.get("ResourceType"):
+            self.ResourceType = params.get("ResourceType")
+        if params.get("ResourceUuid"):
+            self.ResourceUuid = params.get("ResourceUuid")
+        if params.get("TagIds"):
+            self.TagIds = params.get("TagIds")
+
+

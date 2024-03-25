f3f93e52d907b5802163037b0660e9407ac992b8
fengyikai <fengyikai@kingsoft.com>
2024-03-25 14:14:02 +0800

    Update SDK

--- ksyun/__init__.py
+++ ksyun/__init__.py
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 
-__version__ = '1.3.7'
+__version__ = '1.3.8'
---
+++ ksyun/client/kingpay/v20240501/models.py
@@ -0,0 +1,18 @@
+from ksyun.common.abstract_model import AbstractModel
+
+class QueryCashWalletActionRequest(AbstractModel):
+    """QueryCashWalletAction请求参数结构体
+    """
+
+    def __init__(self):
+        r"""获取用户账户余额
+        :param subject: 1:金山云，2：边缘云
+        :type PathPrefix: Int
+        """
+        self.subject = None
+
+    def _deserialize(self, params):
+        if params.get("subject"):
+            self.subject = params.get("subject")
+
+
